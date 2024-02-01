624dfec49aa25c40ab07771dfc0d17ab543a411f
Paula Fenner <fenner.paula@gmail.com>
2024-02-01 15:19:20 +0100

    First commit - basic requirements

---
+++ script.js
@@ -0,0 +1,52 @@
+// DOM elements
+const inputFieldEl = document.getElementById('input-field');
+const addBtnEl = document.getElementById('add-btn');
+const deleteBtnEl = document.getElementById('delete-btn');
+const choresListEl = document.getElementById('chores-list');
+
+let choresList = [];
+
+addBtnEl.addEventListener('click', function () {
+  addChores();
+  clearFieldValue();
+  renderChores();
+});
+
+deleteBtnEl.addEventListener('click', function () {
+  deleteAllChores();
+});
+
+function addChores() {
+  let chore = inputFieldEl.value.trim();
+  if (chore !== '' && !choresList.includes(chore)) {
+    choresList.push(chore);
+    /* localStorage.setItem('choresList', JSON.stringify(choresList)); */
+  } else if (chore === '') {
+    alert('Your chores list is empty. Please write a chore');
+  } else {
+    alert('This chore was already added to your list');
+  }
+}
+
+function clearFieldValue() {
+  inputFieldEl.value = '';
+}
+
+function deleteAllChores() {
+  choresList = [];
+  choresListEl.innerHTML = '';
+  localStorage.clear();
+}
+
+function renderChores() {
+  let listedChore = '';
+  for (let i = 0; i < choresList.length; i++) {
+    listedChore += `
+    <li>${choresList[i]}</li>
+    `;
+  }
+  choresListEl.innerHTML = listedChore;
+}
+
+/* TESTING
+console.log(choresList); */
